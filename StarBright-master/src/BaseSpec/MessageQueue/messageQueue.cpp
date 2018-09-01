2295ee864c0d2a6f531ddb47783317a77e6754ea
Onassis Debrah <debrahonas@gmail.com>
2018-09-01 10:14:59 +0200

    Defined messageQueue

---
+++ StarBright-master/src/BaseSpec/MessageQueue/messageQueue.cpp
@@ -0,0 +1,10 @@
+/*
+ * messageQueue.cpp
+ *
+ *  Created on: 1 Sep 2018
+ *      Author: User
+ */
+
+
+
+
---
+++ StarBright-master/src/BaseSpec/MessageQueue/messageQueue.h
@@ -0,0 +1,15 @@
+/*
+ * messageQueue.h
+ *
+ *  Created on: 1 Sep 2018
+ *      Author: User
+ */
+
+#ifndef BASESPEC_MESSAGEQUEUE_MESSAGEQUEUE_H_
+#define BASESPEC_MESSAGEQUEUE_MESSAGEQUEUE_H_
+
+
+
+
+
+#endif /* BASESPEC_MESSAGEQUEUE_MESSAGEQUEUE_H_ */
